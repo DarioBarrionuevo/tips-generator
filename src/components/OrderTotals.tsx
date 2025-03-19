b220543ddd146f77adbf5099788d3b282b15a388
DarioBarrionuevo <dariobafran@gmail.com>
2025-03-19 11:52:41 +0100

    Add subtotal

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,15 +1,46 @@
-import { OrderItem } from "../types"
+import { useMemo } from "react";
+import { formatCurrency } from "../helpers";
+import { OrderItem } from "../types";
 
 type OrderTotalsProps = {
-    order: OrderItem[],
-    tip: number,
-    placeOrder: () => void
-}
+  order: OrderItem[];
+  tip: number;
+  placeOrder: () => void;
+};
 
-export default function OrderTotals({order, tip, placeOrder} : OrderTotalsProps) {
+export default function OrderTotals({
+  order,
+  tip,
+  placeOrder,
+}: OrderTotalsProps) {
+  const subTotalAmount = useMemo(
+    () => order.reduce((total, item) => total + item.quantity * item.price, 0),
+    [order]
+  );
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="space-y-3">
+        <h2 className="font-black text-2xl">Totales y Propina:</h2>
+        <p>
+          Subtotal a pagar: {""}
+          <span className="font-bold">{formatCurrency(subTotalAmount)}</span>
+        </p>
+        <p>
+          Propina: {""}
+          <span className="font-bold">{formatCurrency(3)}</span>
+        </p>
+        <p>
+          Total a Pagar: {""}
+          <span className="font-bold">{formatCurrency(5)}</span>
+        </p>
+      </div>
+      <button
+        className="w-full bg-black p-3 uppercase hover:cursor-pointer text-white font-bold mt-10 disabled:opacity-10"
+        disabled={false}
+        onClick={placeOrder}
+      >
+        Guardar Orden
+      </button>
+    </>
+  );
 }
