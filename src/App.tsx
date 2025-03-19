e37dc629fdc552e391c0c605adf74f67cb327c67
DarioBarrionuevo <dariobafran@gmail.com>
2025-03-19 09:24:58 +0100

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import "./App.css";
-
 function App() {
-  return <>Hola mundo</>;
+  return (
+    <>
+      <h1 className="text-8xl font-black bg-amber-700">Hola mundo</h1>;
+      <h1 className="text-lg">Hola mundo2</h1>;
+    </>
+  );
 }
 
 export default App;
